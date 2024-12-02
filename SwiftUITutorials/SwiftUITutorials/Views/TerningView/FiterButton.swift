d4c1aeb9189bb7097474021588c5a5c1a05d9f7c
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-12-02 23:29:03 +0900

    [Design] - 필터링 버튼 View 그리기

---
+++ SwiftUITutorials/SwiftUITutorials/Views/TerningView/FiterButton.swift
@@ -0,0 +1,32 @@
+//
+//  FiterButton.swift
+//  SwiftUITutorials
+//
+//  Created by 이명진 on 11/30/24.
+//
+
+import SwiftUI
+
+struct FiterButton: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Image(.icFilter)
+            
+            Text("필터링")
+                .font(.button3)
+                .foregroundStyle(.terningMain)
+            
+        }.padding(EdgeInsets(top: 1, leading: 2, bottom: 1, trailing: 9))
+            .frame(width: 80.adjusted, height: 30.adjustedH)
+            .cornerRadius(5)
+            .overlay(
+            RoundedRectangle(cornerRadius: 5)
+            .inset(by: 0.50)
+            .stroke(.terningMain)
+        )
+    }
+}
+
+#Preview {
+    FiterButton()
+}
